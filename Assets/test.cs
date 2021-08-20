0e7eb302ade6d5b48e09d4a335e0d107c9f35046
DESKTOP-IAEPM7R\vantan <04022005rikuriku@gmail.com>
2021-08-20 13:17:42 +0900

    ひらがなから英語に変えただけ

--- Assets/test.cs
+++ Assets/test.cs
@@ -7,7 +7,7 @@ public class test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("スタート");
+        Debug.Log("start");
     }
 
     // Update is called once per frame
